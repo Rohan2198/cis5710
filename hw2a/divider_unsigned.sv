5b1e57979f63ca732cd1fe8ced119d123555a9cc
Rohan Nagesh <rnagesh@seas.upenn.edu>
2024-02-11 14:33:52 +0000

    hw2a

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -4,34 +4,6 @@
 
 // quotient = dividend / divisor
 
-module divider_unsigned (
-    input  wire [31:0] i_dividend,
-    input  wire [31:0] i_divisor,
-    output wire [31:0] o_remainder,
-    output wire [31:0] o_quotient
-);
-    wire [31:0] i_remainder;
-    wire [31:0] i_quotient; 
-    wire [31:0] o_dividend;
-    
-    genvar i;
-    generate
-        for (i = 0; i < 32; i = i + 1) begin : div_loop
-            divu_1iter d (
-                .i_dividend(i_dividend),
-                .i_divisor(i_divisor),
-                .i_remainder(i_remainder),
-                .i_quotient(i_quotient),
-                .o_dividend(o_dividend),
-                .o_remainder(o_remainder),
-                .o_quotient(o_quotient)
-            );
-        end
-    endgenerate
-endmodule
-
-
-
 module divu_1iter (
     input  wire [31:0] i_dividend,
     input  wire [31:0] i_divisor,
@@ -57,4 +29,44 @@ module divu_1iter (
     assign o_quotient = (x) ? {i_quotient[30:0], 1'b0} : {i_quotient[30:0], 1'b1}; 
     assign o_remainder = (x) ? m_remainder3 : (m_remainder3 - i_divisor);
     assign o_dividend = {i_dividend[30:0], 1'b0};
+endmodule
+
+module divider_unsigned (
+    input  wire [31:0] i_dividend,
+    input  wire [31:0] i_divisor,
+    output wire [31:0] o_remainder,
+    output wire [31:0] o_quotient
+);
+
+    wire [31:0] i_remainder;
+    wire [31:0] i_quotient ;
+
+    wire [31:0] remainder;
+    wire [31:0] quotient ;
+    wire [31:0] dividend ;   
+    
+    genvar i;
+    always_comb begin
+	
+         for (i = 0; i < 32; i = i + 1) begin 
+            divu_1iter d (
+                .i_dividend(i_dividend),
+                .i_divisor(i_divisor),
+                .i_remainder(i_remainder),
+                .i_quotient(i_quotient),
+                .o_dividend(dividend),
+                .o_remainder(remainder),
+                .o_quotient(quotient)
+            );
+		
+        
+i_dividend = dividend;
+i_remainder = remainder;
+i_quotient = quotient;
+end
+end
+assign o_remainder = remainder;
+assign o_quotient = quotient;
+
+
 endmodule
