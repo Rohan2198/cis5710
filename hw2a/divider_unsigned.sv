7a43c275a9fae82f0de6deedbf470e510a87a024
Rohan Nagesh <rnagesh@seas.upenn.edu>
2024-02-10 21:15:56 -0500

    hw2a

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -10,9 +10,12 @@ module divider_unsigned (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-
-    // TODO: your code here
-
+    genvar i;
+    wire [31:0] i_remainder, wire [31:0] i_quotient, wire [31:0] o_dividend;
+        for(i =0; i<32; i = i+1)begin
+        divu_1iter d (.i_dividend(i_dividend),.i_divisor(i_divisor),i_remainder,
+                        i_quotient,o_dividend,.o_remainder(o_remainder),.o_quotient(o_quotient));
+        end
 endmodule
 
 
@@ -25,19 +28,16 @@ module divu_1iter (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-  /*
-    for (int i = 0; i < 32; i++) {
-        remainder = (remainder << 1) | ((dividend >> 31) & 0x1);
-        if (remainder < divisor) {
-            quotient = (quotient << 1);
-        } else {
-            quotient = (quotient << 1) | 0x1;
-            remainder = remainder - divisor;
-        }
-        dividend = dividend << 1;
-    }
-    */
-
-    // TODO: your code here
-
+    wire [31:0] m_remainder = 32'b0;
+    always_comb begin
+        m_remainder = (i_remainder << 1) | ((i_dividend >> 31) & 0x1);
+        if (m_remainder < i_divisor) begin
+            o_quotient = (i_quotient << 1);
+        end
+        else begin
+            o_quotient = (i_quotient << 1) | 0x1;
+            o_remainder = m_remainder - i_divisor;
+        end
+        o_dividend = i_dividend << 1;
+    end
 endmodule
