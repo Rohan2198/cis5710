83d3a2ebb50f04555bd87994891049d3764c5302
Rohan Nagesh <rnagesh@seas.upenn.edu>
2024-03-16 20:29:08 +0000

    Added files

--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -108,12 +108,12 @@ module cla
 
    assign carry[0] = cin; 
 
-   wire [3:0] g_grp, p_grp;
-   wire [2:0] car_grp;
+   wire [3:0] final_g, final_p;
+   wire [2:0] final_carry;
 
-   assign carry[24] = car_grp[2]; 
-   assign carry[16] = car_grp[1]; 
-   assign carry[8] = car_grp[0]; 
+   assign carry[24] = final_carry[2]; 
+   assign carry[16] = final_carry[1]; 
+   assign carry[8] = final_carry[0]; 
 
    genvar i;
 
@@ -121,15 +121,15 @@ module cla
       gp1 g_mod(.a(a[i]), .b(b[i]), .g(g[i]), .p(p[i]));
    end
 
-    gp8 gp8_1(.gin(g[7:0]), .pin(p[7:0]), .cin(carry[0]), .gout(g_grp[0]), .pout(p_grp[0]), .cout(carry[7:1]));
+    gp8 gp8_1(.gin(g[7:0]), .pin(p[7:0]), .cin(carry[0]), .gout(final_g[0]), .pout(final_p[0]), .cout(carry[7:1]));
 
-    gp8 gp8_2(.gin(g[15:8]), .pin(p[15:8]), .cin(car_grp[0]), .gout(g_grp[1]), .pout(p_grp[1]), .cout(carry[15:9]));
+    gp8 gp8_2(.gin(g[15:8]), .pin(p[15:8]), .cin(final_carry[0]), .gout(final_g[1]), .pout(final_p[1]), .cout(carry[15:9]));
 
-    gp8 gp8_3(.gin(g[23:16]), .pin(p[23:16]), .cin(car_grp[1]), .gout(g_grp[2]), .pout(p_grp[2]), .cout(carry[23:17]));
+    gp8 gp8_3(.gin(g[23:16]), .pin(p[23:16]), .cin(final_carry[1]), .gout(final_g[2]), .pout(final_p[2]), .cout(carry[23:17]));
 
-    gp8 gp8_4(.gin(g[31:24]), .pin(p[31:24]), .cin(car_grp[2]), .gout(g_grp[3]), .pout(p_grp[3]), .cout(carry[31:25]));
+    gp8 gp8_4(.gin(g[31:24]), .pin(p[31:24]), .cin(final_carry[2]), .gout(final_g[3]), .pout(final_p[3]), .cout(carry[31:25]));
 
-    gp4 gp4_final(.gin(g_grp[3:0]), .pin(p_grp[3:0]), .cin(carry[0]), .gout(g31_0), .pout(p31_0), .cout(car_grp[2:0]));
+    gp4 gp4_final(.gin(final_g[3:0]), .pin(final_p[3:0]), .cin(carry[0]), .gout(g31_0), .pout(p31_0), .cout(final_carry[2:0]));
 
 
    for(i = 0; i < 32; i = i + 1) begin
